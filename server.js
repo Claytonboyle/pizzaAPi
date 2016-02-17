37ad1d50d24ef8c2f2bdcd2800dec006627c8794
Clayton Boyle <clayton.boyle@gmail.com>
2016-02-17 15:03:51 -0700

    basic server

---
+++ server.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+
+var PORT = 3000 || process.ENV.PORT;
+//var app = express();  //or var app = require('express')();
+
+//midleware funciton below, 
+
+app.use(logger('dev'));
+
+//retrieves data as a nice JSON object
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+
+
+app.listen(PORT, function(PORT){
+	console.log('Your server is up on port ',PORT);
+})*/
+
+
